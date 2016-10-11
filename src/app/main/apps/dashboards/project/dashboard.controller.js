f6faabecb696bc8ce965c978b7e42ecb0686c93d
Lenny Castaneda <lenny.castaneda@gmail.com>
2016-10-11 16:00:31 -0700

    worked on getting driver detail view setup...still needs work. Can't decide to make a modal to add to detail or create separate HTML view page to add new patient/driver/order.

--- src/app/main/apps/dashboards/project/dashboard.controller.js
+++ src/app/main/apps/dashboards/project/dashboard.controller.js
@@ -5,13 +5,15 @@
         .module('cannDash')
         .controller('DashboardController', DashboardController);
 
-    DashboardController.$inject = ['$http', '$q', 'dashboardFactory', 'toastr'];
+    DashboardController.$inject = ['$http', '$q', 'dashboardFactory', 'toastr', 'driverFactory'];
 
     /* @ngInject */
-    function DashboardController($http, $q, dashboardFactory, toastr) {
+    function DashboardController($http, $q, dashboardFactory, toastr, driverFactory) {
         var vm = this;
         
         vm.dashData = [];
+        vm.newDriver = {};
+
 
         activate();
 
@@ -23,5 +25,10 @@
                 vm.dashData = response;
 			});
         }
+
+        driverFactory.addDriver().then(
+            function(response) {
+                vm.newDriver = response;
+            });
     }
 })();
